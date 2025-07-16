1bd64aadd93fd3f8aa3c45174874df4dd7a0191a
Naresh Air <naresh.air.2053@gmail.com>
2025-07-15 21:00:30 -0400

    coursera11 assignment

---
+++ assi.py
@@ -0,0 +1,13 @@
+# Lists from Step 1 (do not modify)
+celsius_temperatures = [0, 10, 25, 32, 100]
+fahrenheit_temperatures = [] 
+
+# Convert each Celsius temperature to Fahrenheit
+for celsius in celsius_temperatures:  # Start the loop (do not modify this line)
+  fahrenheit = F = (celsius * 9/5) + 32 ##### Insert code here
+
+  fahrenheit_temperatures.append(fahrenheit)  # Append to the list (do not modify this line)
+
+# Print the results (including the output from Step 1 - do not modify)
+print("Celsius Temperatures:", celsius_temperatures)
+print("Fahrenheit Temperatures:", fahrenheit_temperatures)
