1838d51da82370c5c45995f42f6f16ee6ec663a2
Naresh Air <naresh.air.2053@gmail.com>
2025-07-16 20:57:24 -0400

    new files

---
+++ rama.py/nar.py
@@ -0,0 +1,6 @@
+exam_scores = [55, 70, 78, 52, 68]
+curve_amount = 10
+# Use a list comprehension to create a new list of curved grades
+curved_grades = [score + curve_amount for score in exam_scores]
+print("Original scores:", exam_scores)
+print("Curved scores:", curved_grades)
