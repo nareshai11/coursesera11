191914ccd90724352cae79012268331ac6c03995
Naresh Air <naresh.air.2053@gmail.com>
2025-07-23 21:03:16 -0400

    new

--- list.py
+++ list.py
@@ -3,6 +3,6 @@ list_length = len(list)
 list.append("Mama")
 print("The number of family members is:", list_length)
 print("Naresh's Father is", list[2])
-print("All family members", list)
+print("All family member", list)
 for family_member in list:
     print("Mui Bandana bollariu 'Dhog  diya'", family_member)
