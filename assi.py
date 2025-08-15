825766201e70d30621fa06d0e2999856d4dbb1fe
Naresh Air <naresh.air.2053@gmail.com>
2025-08-14 20:45:04 -0400

    new

--- assi.py
+++ assi.py
@@ -4,7 +4,7 @@ fahrenheit_temperatures = []
 
 # Convert each Celsius temperature to Fahrenheit
 for celsius in celsius_temperatures:  # Start the loop (do not modify this line)
-  fahrenheit = F = (celsius * 9/5) + 32 ##### Insert code here
+  fahrenheit = F = (celsius * 9/5) + 32 ## Insert code here
 
   fahrenheit_temperatures.append(fahrenheit)  #Append to the list (do not modify this line)
 
