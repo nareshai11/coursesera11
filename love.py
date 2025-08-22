453229ed21c731a00590fb0417e880022ea426f7
Naresh Air <naresh.air.2053@gmail.com>
2025-08-22 19:12:05 -0400

    new

--- love.py
+++ love.py
@@ -2,6 +2,6 @@ guru = str(input("Enter guru's name:"))
 print("I love you", guru)
 def myfun():
     guru = "Sadguru"
-    print ("I love very you much", guru)
+    print ("I love very you", guru)
 myfun()
 print(guru)
