ae3d4003a9edbe5e4c857cf55c6c12148a4d5c07
Naresh Air <naresh.air.2053@gmail.com>
2025-09-01 21:02:10 -0400

    new

--- even11.py
+++ even11.py
@@ -1,5 +1,5 @@
 max_value = 50
 for max_value in range (0,max_value+1):
     if max_value % 3 == 0 and max_value % 4 == 0:
-        print("Value is :", max_value)
+        print("Value is:", max_value)
     max_value += 1
