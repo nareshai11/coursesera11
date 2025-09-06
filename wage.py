37a37ff90fef84b0805cb5af354435aa21f96262
Naresh Air <naresh.air.2053@gmail.com>
2025-09-05 20:35:55 -0400

    new

--- wage.py
+++ wage.py
@@ -1,4 +1,4 @@
 hour = int(input("Enter worked hour:"))
 hourly_rate = float(input("Enter hourly pay:"))
 gross_pay = hour * hourly_rate
-print ("Your  gross pay", gross_pay)
+print ("Your gross pay", gross_pay)
