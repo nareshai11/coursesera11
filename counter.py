58b909033b59555246a024eee757dbffb81605f3
Naresh Air <naresh.air.2053@gmail.com>
2025-09-08 21:13:12 -0400

    new

--- counter.py
+++ counter.py
@@ -2,7 +2,7 @@ counter = 10
 while counter >= 0:
     print("Current value of counter is:", counter)
     if counter == 5:
-        print("Halfway point  reached!")
+        print("Halfway point reached!")
     counter -= 1
     print ("I Naresh")
     
