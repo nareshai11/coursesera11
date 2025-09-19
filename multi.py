d6b3c3e73c395cfc1384875474581c08778b1c37
Naresh Air <naresh.air.2053@gmail.com>
2025-09-18 20:16:49 -0400

    new

--- multi.py
+++ multi.py
@@ -1,5 +1,5 @@
 #generating a classic multiplication table
 for i in range(1,11):
     for j in range(1,11):
-        print("classsic  multiplication is", "*", j, "=", i * j, end="\t")
+        print(" classsic  multiplication is", "*", j, "=", i * j, end="\t")
     print()
