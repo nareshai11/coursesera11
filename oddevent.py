01f916060b009ea93528d5d28247d42839b9a5a1
Naresh Air <naresh.air.2053@gmail.com>
2025-10-11 20:47:17 -0400

    new

--- oddevent.py
+++ oddevent.py
@@ -1,6 +1,6 @@
 numbers = [3, 9, 1, 10, 5, 2, 8]
 for number in numbers:
     if number % 2 == 0:
-        print (number, "is even")
+        print (number, "is  even")
     else:
         print(number,"is odd")
